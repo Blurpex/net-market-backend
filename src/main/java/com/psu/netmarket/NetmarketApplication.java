a8b601ec8f063f7b1c875e7fdb0d7799b98afe5f
Blurpex <harizahmad100@gmail.com>
2023-05-11 15:32:48 -0400

    initial commit

---
+++ src/main/java/com/psu/netmarket/NetmarketApplication.java
@@ -0,0 +1,13 @@
+package com.psu.netmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetmarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psu/netmarket/NetmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psu.netmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
