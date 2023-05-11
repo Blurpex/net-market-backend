1165ea0ee63fcf2fdff83c895b19d868e69a3c13
Blurpex <harizahmad100@gmail.com>
2023-05-11 15:58:49 -0400

    customer model

--- src/main/java/com/psu/netmarket/model/Customer.java
+++ src/main/java/com/psu/netmarket/model/Customer.java
@@ -1,4 +1,45 @@
 package com.psu.netmarket.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customer")
 public class Customer {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private byte[] password;
+
+    @Column(name = "phone_number")
+    private Integer phoneNumber;
+
+    @Column(name="street_address")
+    private String streetAddress;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
 }
