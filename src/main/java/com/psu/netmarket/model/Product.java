c9f80e529f3150adce97e66d3e752937a2c3a9ed
Blurpex <harizahmad100@gmail.com>
2023-05-11 16:12:16 -0400

    product model

---
+++ src/main/java/com/psu/netmarket/model/Product.java
@@ -0,0 +1,40 @@
+package com.psu.netmarket.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="product")
+public class Product {
+
+    @Id
+    @Column(name = "upc")
+    private Long upc;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "price", precision = 10, scale = 2, nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "inventory", nullable = false)
+    private Integer inventory;
+}
