8b91ad7328bb0121320705138c07e19584a1e23c
Haris Ahmad <harizahmad100@gmail.com>
2023-05-13 00:51:21 -0400

    more customer controller functionality

--- src/main/java/com/psu/netmarket/controller/CustomerController.java
+++ src/main/java/com/psu/netmarket/controller/CustomerController.java
@@ -2,7 +2,6 @@ package com.psu.netmarket.controller;
 
 import com.psu.netmarket.exception.NotFoundException;
 import com.psu.netmarket.model.Customer;
-import com.psu.netmarket.repository.CustomerDAO;
 import com.psu.netmarket.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -31,6 +30,21 @@ public class CustomerController {
         if(customer == null)
             throw new NotFoundException("customer " + userName + " not found");
         return customer;
+    }
+
+    @PostMapping("/post/customer")
+    public void insertCustomer(@RequestBody Customer customer) {
+        customerService.insertCustomer(customer);
+    }
+
+    @PutMapping("/put/customer")
+    public void updateCustomer(@RequestBody Customer customer) {
+        customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping("/delete/{userName}")
+    public void deleteCustomer(@PathVariable String userName) {
+        customerService.deleteCustomer(userName);
     }
 
 }
